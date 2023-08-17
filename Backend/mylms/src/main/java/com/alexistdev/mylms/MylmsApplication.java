381203006ab94f3435dbc119ebab79264341339d
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2023-08-17 14:14:15 +0700

    Backend MyLMS v.1.0

---
+++ Backend/mylms/src/main/java/com/alexistdev/mylms/MylmsApplication.java
@@ -0,0 +1,13 @@
+package com.alexistdev.mylms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MylmsApplication.class, args);
+    }
+
+}
---
+++ Backend/mylms/src/test/java/com/alexistdev/mylms/MylmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexistdev.mylms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
